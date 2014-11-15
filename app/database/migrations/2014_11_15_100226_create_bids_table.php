6e3523e99b1548f2d5faad8227ac10069ae85aec
Panida <cp.panida@gmailcom>
2014-11-15 19:11:11 +0700

    add model -bid manager -bid -item

---
+++ app/database/migrations/2014_11_15_100226_create_bids_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBidsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('bids', function ($table) {
+			$table->increments('id');
+			$table->double('maxBid');
+			$table->double('increment');
+			$table->double('bid');
+			$table->string('type');
+			$table->integer('bidManagerId')->unsigned();
+			$table->foreign('bidManagerId')->references('id')->on('bid_managers');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('bids');
+		
+	}
+
+}
