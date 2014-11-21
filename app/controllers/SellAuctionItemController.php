399886366185653c087ccef4fc2d7c411dafc548
plkumjorn <plkumjorn@gmail.com>
2014-11-21 21:43:55 +0700

    - catch exception max size upload file

--- app/controllers/SellAuctionItemController.php
+++ app/controllers/SellAuctionItemController.php
@@ -18,54 +18,62 @@ class SellAuctionItemController extends Controller
 		return View::make('item.sellAuctionItem');		
 	}
 	public function createAuctionItem(){
-		//'id','name','picture','price','brand','model','volumn','property','size','quantity','quality','defect','returnPolicy','returnFee','shipping','tax','others','type','endDateTime','amount','bidManagerId','sellerId'
-		$input = Input::all();
-		
-		$file = Input::file('picture');
-		$destinationPath = 'upload/';
-		$filename = $file->getClientOriginalName();
-		Input::file('picture')->move($destinationPath, $filename);
-		// $path = '../public/'.$destinationPath.$filename;
+		$file_max = ini_get('upload_max_filesize');
 
-		// $this->item->name = $input['name'];
-		$this->item->picture = $filename;
-		$this->item->quantity = 1;
-		// $this->item->price = $input['price'];
-		// $this->item->brand = $input['brand'];
-		// $this->item->model = $input['model'];
-		// $this->item->volumn = $input['volumn'];
-		// $this->item->property = $input['property'];
-		// $this->item->size = $input['size'];
-		// $this->item->quantity = $input['quantity'];
-		// $this->item->quality = $input['quality'];
-		//'defect','returnPolicy','returnFee','shipping','tax','others','type','endDateTime','amount','bidManagerId',
-		// $this->item->defect = $input['defect'];
-		// $this->item->returnPolicy = $input['returnPolicy'];
-		// $this->item->returnFee = $input['returnFee'];
-		// $this->item->shipping = $input['shipping'];
-		// $this->item->tax = $input['tax'];
-		// $this->item->others = $input['others'];
-		$shipping = array('แบบด่วน' => $input['quick'], 'แบบมาตรฐาน' => $input['standard'], 'แบบประหยัด' => $input['cheap']);
-		$this->item->shipping = json_encode($shipping);
-		$this->item->type = 'auction';
-		$this->item->endDateTime = $input['endDate'].' '.$input['endTime'];
-		$this->item->bidManagerId = NULL;
-		// $this->item->amount=NULL;
-		$this->item->sellerId = Auth::user()->id;
-		// $this->item->id = $input['id'];
-		
-		if (!$this->item->fill($input)->isValid()) {
-			return Redirect::back()->withInput()->withErrors($this->item->errors)->with('error',$this->item->errors);
+        try{
+
+			//'id','name','picture','price','brand','model','volumn','property','size','quantity','quality','defect','returnPolicy','returnFee','shipping','tax','others','type','endDateTime','amount','bidManagerId','sellerId'
+			$input = Input::all();
+			
+			$file = Input::file('picture');
+			$destinationPath = 'upload/';
+			$filename = $file->getClientOriginalName();
+			Input::file('picture')->move($destinationPath, $filename);
+			// $path = '../public/'.$destinationPath.$filename;
+
+			// $this->item->name = $input['name'];
+			$this->item->picture = $filename;
+			$this->item->quantity = 1;
+			// $this->item->price = $input['price'];
+			// $this->item->brand = $input['brand'];
+			// $this->item->model = $input['model'];
+			// $this->item->volumn = $input['volumn'];
+			// $this->item->property = $input['property'];
+			// $this->item->size = $input['size'];
+			// $this->item->quantity = $input['quantity'];
+			// $this->item->quality = $input['quality'];
+			//'defect','returnPolicy','returnFee','shipping','tax','others','type','endDateTime','amount','bidManagerId',
+			// $this->item->defect = $input['defect'];
+			// $this->item->returnPolicy = $input['returnPolicy'];
+			// $this->item->returnFee = $input['returnFee'];
+			// $this->item->shipping = $input['shipping'];
+			// $this->item->tax = $input['tax'];
+			// $this->item->others = $input['others'];
+			$shipping = array('แบบด่วน' => $input['quick'], 'แบบมาตรฐาน' => $input['standard'], 'แบบประหยัด' => $input['cheap']);
+			$this->item->shipping = json_encode($shipping);
+			$this->item->type = 'auction';
+			$this->item->endDateTime = $input['endDate'].' '.$input['endTime'];
+			$this->item->bidManagerId = NULL;
+			// $this->item->amount=NULL;
+			$this->item->sellerId = Auth::user()->id;
+			// $this->item->id = $input['id'];
+			
+			if (!$this->item->fill($input)->isValid()) {
+				return Redirect::back()->withInput()->withErrors($this->item->errors)->with('error',$this->item->errors);
+			}
+			// echo "<pre>";
+			// print_r($path);
+			// echo "</pre>";
+
+			// return View::make('emptypage');
+			$this->item->save();
+			$newItem = Item::orderBy('id', 'desc')->first();
+			return Redirect::to('item/'.$newItem->id)->with('notice','ระบบเพิ่มสินค้าของท่านเรียบร้อยแล้วค่ะ');		
+			//return Redirect::action('SellAuctionItemController@sellAuctionItem')->with('notice','ระบบเพิ่มสินค้าของคุณเรียบร้อยแล้วค่ะ'.$this->item->id);
 		}
-		// echo "<pre>";
-		// print_r($path);
-		// echo "</pre>";
-
-		// return View::make('emptypage');
-		$this->item->save();
-		$newItem = Item::orderBy('id', 'desc')->first();
-		return Redirect::to('item/'.$newItem->id)->with('notice','ระบบเพิ่มสินค้าของท่านเรียบร้อยแล้วค่ะ');		
-		//return Redirect::action('SellAuctionItemController@sellAuctionItem')->with('notice','ระบบเพิ่มสินค้าของคุณเรียบร้อยแล้วค่ะ'.$this->item->id);
+		catch(Exception $e){
+			return Redirect::back()->withInput()->withErrors($this->item->errors)->with('error','The file size should be lower than '.$file_max. 'B');
+		}
 	}
 
 	public function deleteAuctionItem($id){
